8cf6798b9562468d82e98a5da895c2a886fa1f11
Eren Gun <70534390+Erengun@users.noreply.github.com>
2025-09-06 07:44:49 -0700

    Initial commit

---
+++ lib/config/theme/theme_logic.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'theme_logic.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$themeLogicHash() => r'589d5a8a5a32bbb1c4c3e791efcf37190cc2472a';
+
+/// See also [ThemeLogic].
+@ProviderFor(ThemeLogic)
+final themeLogicProvider =
+    AutoDisposeNotifierProvider<ThemeLogic, ThemeUiModel>.internal(
+  ThemeLogic.new,
+  name: r'themeLogicProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$themeLogicHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ThemeLogic = AutoDisposeNotifier<ThemeUiModel>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
